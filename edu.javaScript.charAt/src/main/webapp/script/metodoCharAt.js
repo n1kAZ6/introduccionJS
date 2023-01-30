c8b763c4aac7b3f2de8e1a83392cb0eb704c809d
niko6 <nikoalvarezzapata@gmail.com>
2023-01-30 09:07:32 +0100

    Ejercicio clase método charAt y secuencia escape

---
+++ edu.javaScript.charAt/src/main/webapp/script/metodoCharAt.js
@@ -0,0 +1,10 @@
+function ejercicioCharAt(){
+	let miNombre = "Nicolás Álvarez Zapata"
+	let misIniciales = "Mis iniciales: "+miNombre.charAt(0)+miNombre.charAt(8)+miNombre.charAt(16);
+	alert(misIniciales);
+}
+function ejercicioSecuencia(){
+	let saltoParrafo="Primer párrafo \nSegundo párrafo \n\"Tercer párrafo con comillas\"";
+	
+	alert(saltoParrafo);
+}
